1f79a850f6a9569330da189f8cb101d9a8a66cb3
FreeBot <121530654+CryptoBotCuckoo@users.noreply.github.com>
2023-01-08 11:42:29 +0800

    Add files via upload
    tradingview分发器

---
+++ dispatch_bot/dct_install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+mkdir -p /root/DistributeCenter
+mv dctbot.zip /root/DistributeCenter
+cd /root/DistributeCenter
+unzip dctbot.zip
+cp -rf dctbot/*.* ./
+rm -rf dctbot.zip dctbot
+cp -rf printMoney.service /lib/systemd/system/
+systemctl daemon-reload
+systemctl enable printMoney.service
+rm -rf printMoney.service
+
+pip3 install waitress
+pip3 install flask
+
+cd -
+rm -rf dct_install.sh 
